ea3ef305669f219583a515c7706d1a462abd30d5
yj <pyeunju88@gmail.com>
2021-06-16 06:21:24 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,16 +3,14 @@ module "s3-bucket" {
   source  = "app.terraform.io/yj-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
+}
 
-
-  bucket = "s3-tf-example-versioning"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
   acl    = "private"
 
-  versioning_inputs = [
-    {
-      enabled = true
-      mfa_delete = null
-    },
-  ]
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
->>>>>>> a32e61a6ad2f6630e0bfeaf84e69fe678c26646d
